bb93e9b47f25b4be105d9a5d369eabea897d213c
Dominik Wysota <Medutenza@gmail.com>
2019-12-17 22:18:39 +0100

    Add Capacity component and method click

---
+++ src/components/Capacity.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Capacity = props => {
+  const capacities = props.capacities.map((capacity, index) => (
+    <div
+      className="capacities"
+      onClick={props.click}
+      key={capacity.id}
+      name={capacity.name}
+      capacity_id={capacity.id}
+      price_modifier={capacity.priceModifier}
+      iditem={props.iditem}
+    >
+      {capacity.name}
+    </div>
+  ));
+  return <div>{capacities}</div>;
+};
+
+export default Capacity;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Items.css";
 import Colors from "./Colors";
+import Capacity from "./Capacity";
 
 class Items extends Component {
   state = {
@@ -20,7 +21,9 @@ class Items extends Component {
         priceModifierCol: item.options[0].values[0].priceModifier,
         colorName: item.options[0].values[0].name,
         colorID: item.options[0].values[0].id,
-        capacityID: item.options[1].values[0].id
+        capacityID: item.options[1].values[0].id,
+        capacityName: item.options[1].values[0].name,
+        priceModifierCap: item.options[1].values[0].priceModifier
       };
       this.setState(prevState => ({
         loaded: true,
@@ -47,14 +50,42 @@ class Items extends Component {
     }));
   };
 
+  clickChangeCapacity = event => {
+    const iditem = event.target.getAttribute("iditem") * 1;
+    const capacityID = event.target.getAttribute("capacity_id") * 1;
+    const capacityName = event.target.getAttribute("name");
+    const priceModifierCap = event.target.getAttribute("price_modifier") * 1;
+    this.setState(prevState => ({
+      choices: prevState.choices.map(obj =>
+        obj.id === iditem
+          ? Object.assign(obj, {
+              capacityID: capacityID,
+              capacityName: capacityName,
+              priceModifierCap: priceModifierCap
+            })
+          : obj
+      )
+    }));
+  };
+
   render() {
     const { choices, loaded } = this.state;
     const shopItems = this.items.map((item, index) => (
       <div className="item" key={item.id}>
         <div className="photo">Photo</div>
         <Colors click={this.clickChangeColor} colors={item.options[0].values} iditem={item.id} />
+        <Capacity
+          click={this.clickChangeCapacity}
+          capacities={item.options[1].values}
+          iditem={item.id}
+        />
         <h1>{loaded && `${choices[index].name} ${choices[index].colorName}`}</h1>
-        <h2>{loaded && choices[index].price + choices[index].priceModifierCol}</h2>
+        <h2>
+          {loaded &&
+            choices[index].price +
+              choices[index].priceModifierCol +
+              choices[index].priceModifierCap}
+        </h2>
       </div>
     ));
     return <>{shopItems}</>;
