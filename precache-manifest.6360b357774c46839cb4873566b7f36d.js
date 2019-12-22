c0cd1c051fec25ec39dc768ad840262f8c170e31
Dominik Wysota <Medutenza@gmail.com>
2019-12-22 20:17:45 +0100

    Updates

---
+++ precache-manifest.6360b357774c46839cb4873566b7f36d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294b063985821f5886b44f2542e870e2",
+    "url": "/iphoneShopReact/index.html"
+  },
+  {
+    "revision": "c406b7f1644b86cd34ae",
+    "url": "/iphoneShopReact/static/css/main.7c9bc10b.chunk.css"
+  },
+  {
+    "revision": "504dc6e7d60e702db6cf",
+    "url": "/iphoneShopReact/static/js/2.cbeb5d79.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/iphoneShopReact/static/js/2.cbeb5d79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c406b7f1644b86cd34ae",
+    "url": "/iphoneShopReact/static/js/main.a27a2f0b.chunk.js"
+  },
+  {
+    "revision": "536433070c84fc4de696",
+    "url": "/iphoneShopReact/static/js/runtime-main.e61b10f9.js"
+  }
+]);
